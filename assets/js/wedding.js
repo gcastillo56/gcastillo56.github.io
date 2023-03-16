59404e4f8ba8866424e3c4af6c256783bfdc4f8c
GCastillo <gcastillo@ucdavis.edu>
2023-03-15 23:55:47 -0600

    Final v1

--- assets/js/wedding.js
+++ assets/js/wedding.js
@@ -29,10 +29,6 @@ $(document).ready(function() {
         $(this).attr("href", "http://wa.me/+523334407261");
         $(this).attr("target", "_blank");
     });
-    $("#whatsapp_dul").on("click", function() {
-        $(this).attr("href", "http://wa.me/+523114115687");
-        $(this).attr("target", "_blank");
-    });
 });
 
 // Get the modal
@@ -105,3 +101,131 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// upload image to github
+function uploadImage(data) {
+    return fetch(
+        `https://api.github.com/repos/gcastillo56/gcastillo56.github.io/contents/assets/photos/${data.name}`, {
+            method: "PUT",
+            headers: {
+                Accept: "application/vnd.github+json",
+                Authorization: `Bearer ${data.token}`,
+            },
+            body: JSON.stringify({
+                message: "upload image from api",
+                content: data.content,
+            }),
+        }
+    ).then((res) => res.json());
+}
+
+function insertImage(src) {
+    const newImage = document.createElement("img");
+    newImage.src = src;
+    document.querySelector(".img").innerHTML = newImage.outerHTML;
+}
+
+function getRandomName(type) {
+    if (type.endsWith("png")) {
+        return [Date.now(), ".png"].join("");
+    }
+    return [Date.now(), ".jpeg"].join("");
+}
+
+function blobToBase64(blob) {
+    return new Promise((resolve) => {
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+            const srcData = fileReader.result;
+            resolve(srcData);
+        };
+        fileReader.readAsDataURL(blob);
+    });
+}
+
+async function parseClipboardData(callback) {
+    const items = await navigator.clipboard.read().catch((err) => {
+        console.error(err);
+    });
+    for (let item of items) {
+        for (let type of item.types) {
+            if (type.startsWith("image/")) {
+                console.log("item-->: ", item);
+                item
+                    .getType(type)
+                    .then(blobToBase64)
+                    .then((srcData) => {
+                        insertImage(srcData);
+                        callback &&
+                            callback({
+                                content: srcData,
+                                name: getRandomName(type),
+                                type: type,
+                            });
+                    });
+                return true;
+            }
+        }
+    }
+}
+
+function handleImageChange(callback) {
+    const $file = document.querySelector(".local");
+    $file.addEventListener("change", (event) => {
+        const selectedfile = event.target.files;
+        if (selectedfile.length > 0) {
+            const [imageFile] = selectedfile;
+            blobToBase64(imageFile).then((srcData) => {
+                insertImage(srcData);
+                callback &&
+                    callback({
+                        content: srcData,
+                        name: imageFile.name,
+                        type: imageFile.type,
+                    });
+            });
+        }
+    });
+
+    document.querySelector(".paste").onclick = () => {
+        parseClipboardData(callback);
+    };
+}
+
+window.onload = () => {
+    const $form = document.querySelector(".form");
+    let image = null;
+    $form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        if (!image) {
+            alert("Image is empty, please select or paste");
+            return;
+        }
+        const formData = new FormData($form);
+        const data = {};
+        for (let [key, value] of formData.entries()) {
+            data[key] = value;
+        }
+
+        uploadImage({
+            ...data,
+            ...image,
+        }).then((res) => {
+            let message = "";
+            if (res.message) {
+                message = res.message;
+            } else {
+                message = `upload success`;
+            }
+            console.log("res", res);
+            document.querySelector(".log").innerHTML = message;
+        });
+    });
+
+    handleImageChange((imageInfo) => {
+        image = {
+            ...imageInfo,
+            content: imageInfo.content.split("base64,")[1],
+        };
+    });
+};
